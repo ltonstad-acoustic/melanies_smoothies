5b926a560c81e43dbeb8d1f233b672b040c5d845
ltonstad-logic <97191934+ltonstad-logic@users.noreply.github.com>
2024-08-05 16:10:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -27,7 +26,6 @@ ingredients_list = st.multiselect(
 )
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response)
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 if ingredients_list:
